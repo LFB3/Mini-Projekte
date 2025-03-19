29b72284d45d2e0ff15fdbe9613a00029d43a4bf
Luca Frauen-Berens <luca_f-b@outlook.de>
2025-03-19 18:32:25 +0100

    Manim vid, dont watch it
    I made a small manim animation for my friends, please dont watch it

--- Manim/main.py
+++ Manim/main.py
@@ -2,7 +2,42 @@ from manim import *
 
 class test(Scene):
     def construct(self):
-        lol = Text("Lol", fill_opacity=0.3)
-        s = RoundedRectangle(color = ORANGE, corner_radius=0.2, )
-        self.play(Write(s), )
-        self.wait()
+        t = Tex("Du ","bist ","Dumm")
+        t[0].set_color(RED)
+        t[1].set_color(GREEN)
+        t[2].set_color(BLUE)
+        t2 = Tex("Ja ","Du ","Niemand ","anderes ","DU ")
+        t2[0].set_color(RED)
+        t2[1].set_color(GREEN)
+        t2[1].weight = SEMIBOLD
+        t2[2].set_color(BLUE)
+        t2[3].set_color(YELLOW)
+        t2[4].set_color(PURPLE)
+        t2[4].weight = ULTRAHEAVY
+        t2[4].font_size = 96
+        t2[4].shift(RIGHT*1)
+        self.play(Write(t[0]))
+        self.wait(0.5)
+        self.play(Write(t[1]))
+        self.wait(0.5)
+        self.play(Write(t[2]))
+        self.wait(2)
+
+        self.play(FadeOut(t))
+        self.play(FadeIn(t2[0:2]))
+        self.wait(2)
+        self.play(DrawBorderThenFill(t2[2:4]))
+        self.wait(2)
+        self.play(Write(t2[4]), run_time=4)
+
+        
+        
+        self.wait(5)
+        paul = Text("Paul-Lennard ich weiß das du das siehst, schick die PPP", font_size=25).to_corner(DL, buff=0.5)
+        paul2 = Text("Paul-Lennard ich weiß das du das siehst, schick die Power Point Präsentation", font_size=25).to_corner(DL, buff=0.5)
+        self.play(FadeIn(paul))
+        self.wait(2)
+        self.play(Transform(paul, paul2))
+        self.wait(6)
+
+        self.play(FadeOut(paul2),FadeOut(t2))        
