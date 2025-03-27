29a02a30c08d700dc18c6c0b624d23525bd03ea7
Luca Frauen-Berens <luca_f-b@outlook.de>
2025-03-27 19:10:31 +0100

    Summary

--- Manim/main.py
+++ Manim/main.py
@@ -1,5 +1,26 @@
 from manim import *
 
+class Axes(Scene):
+    def construct(self):
+        axes = Axes()
+
+
+
+
+
+
+      
+        
+
+class ContinuousMotion(Scene):
+    def construct(self):
+        func = lambda pos: np.sin(pos[0] / 2) * UR + np.cos(pos[1] / 2) * LEFT
+        stream_lines = StreamLines(func, stroke_width=2, max_anchors_per_line=30)
+        self.add(stream_lines)
+        stream_lines.start_animation(warm_up=False, flow_speed=1.5)
+        self.wait(stream_lines.virtual_time / stream_lines.flow_speed)
+
+
 class test(Scene):
     def construct(self):
         t = Tex("Du ","bist ","Dumm")
@@ -40,4 +61,33 @@ class test(Scene):
         self.play(Transform(paul, paul2))
         self.wait(6)
 
-        self.play(FadeOut(paul2),FadeOut(t2))        
+        self.play(FadeOut(paul2),FadeOut(t2))     
+
+
+class demo(Scene):
+    def construct(self):
+        t = Text("Was haltet ihr")
+        t2 = Text("von diesen Animationen?")
+        t3 = Text("?", color = RED, font_size=150)
+        t4 = Text("?", color = RED, font_size=100000)
+        s = Circle(color = RED)
+        s2 = Circle(color = RED, fill_opacity=0.5, radius=4)
+        r = Rectangle(height=4, width=4, color=BLUE, fill_opacity=0.05)
+        self.wait(1)
+        self.play(Write(t))
+        self.wait(0.5)
+        self.play(Transform(t, t2))
+        self.wait(1)
+        self.play(Transform(t, t3))
+        self.play(Transform(t, t4))
+        self.wait(1)
+        self.play(FadeOut(t), FadeIn(s))
+        self.wait(1)
+        self.play(Transform(s, s2),DrawBorderThenFill(r), run_time=2)
+        self.wait(1)
+        r3 = []
+        for i in range(10):
+            r3.append(Rectangle(height=4, width=4, color=BLUE, fill_opacity=i/10))
+            self.play(Transform(r, r3[i]))
+        self.wait(2)
+        
